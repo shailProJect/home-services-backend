413dc62ed81db3fb87c8fbfe8fa9940fd8ba7a51
shailesh <shailesh.choudhary.EXT@nmims.edu>
2026-05-25 15:55:14 +0530

    wdfdwf

--- home-services-backend/src/main/java/com/homeservices/config/FirebaseConfig.java
+++ home-services-backend/src/main/java/com/homeservices/config/FirebaseConfig.java
@@ -3,33 +3,32 @@ package com.homeservices.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-
 import jakarta.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
-  @PostConstruct
-  public void init() {
+    @Value("${FIREBASE_CREDENTIALS}")
+    private String firebaseCredentials;
 
-    try {
+    @PostConstruct
+    public void init() throws IOException {
 
-      InputStream serviceAccount =
-          getClass().getClassLoader().getResourceAsStream("firebase-service-account.json");
+        InputStream serviceAccount =
+                new ByteArrayInputStream(firebaseCredentials.getBytes());
 
-      FirebaseOptions options = FirebaseOptions.builder()
-          .setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
 
-      if (FirebaseApp.getApps().isEmpty()) {
-        FirebaseApp.initializeApp(options);
-      }
-
-    } catch (Exception e) {
-      e.printStackTrace();
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
     }
-  }
 }
